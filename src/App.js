a5b73455d762359bbba1fd874bc2ae4946a68a56
ankithlg <you@ankithalgakash609@gmail.com>
2025-05-17 13:00:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task.trim()) {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={i}>
+            {todo} <button onClick={() => removeTodo(i)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
